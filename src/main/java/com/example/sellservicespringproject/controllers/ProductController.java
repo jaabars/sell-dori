64c808bc2ecd6b3d3ef7bbcc7acf753d9246ae76
doorbiy <tdoorbiy@gmail.com>
2021-09-28 18:36:10 +0600

    created ProductController

---
+++ src/main/java/com/example/sellservicespringproject/controllers/ProductController.java
@@ -0,0 +1,37 @@
+package com.example.sellservicespringproject.controllers;
+
+import com.example.sellservicespringproject.models.dtos.ProductDto;
+import com.example.sellservicespringproject.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/vq/product")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping("/save")
+    public ResponseEntity<?> saveProduct(@RequestHeader String token, @RequestBody ProductDto productDto) {
+        return productService.saveProduct(token, productDto);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<?> updateProduct(@RequestHeader String token, @RequestBody ProductDto productDto) {
+        return productService.saveProduct(token, productDto);
+    }
+
+    @GetMapping("/getByBarcode")
+    public ResponseEntity<?> getProductByBarcode (@RequestHeader String token, @RequestParam String barcode) {
+        return productService.getProductByBarcode(token, barcode);
+    }
+
+    @GetMapping("/getAllProducts")
+    public List<ProductDto> getAllProducts(@RequestHeader String token) {
+        return productService.getAllProducts(token);
+    }
+}
