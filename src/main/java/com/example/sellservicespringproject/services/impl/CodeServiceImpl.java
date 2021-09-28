be899cc51c7d838a63631d43c677c81a659bcdaa
doorbiy <tdoorbiy@gmail.com>
2021-09-28 12:30:30 +0600

    deleted randomCode, findUserCode methods, modified saveCode method, created sendCode method

--- src/main/java/com/example/sellservicespringproject/services/impl/CodeServiceImpl.java
+++ src/main/java/com/example/sellservicespringproject/services/impl/CodeServiceImpl.java
@@ -1,12 +1,20 @@
 package com.example.sellservicespringproject.services.impl;
 
 import com.example.sellservicespringproject.dao.CodeRepo;
+import com.example.sellservicespringproject.mappers.CodeMapper;
+import com.example.sellservicespringproject.mappers.UserMapper;
+import com.example.sellservicespringproject.models.dtos.CodeDto;
+import com.example.sellservicespringproject.models.dtos.UserDto;
 import com.example.sellservicespringproject.models.entities.Code;
-import com.example.sellservicespringproject.models.entities.User;
 import com.example.sellservicespringproject.models.enums.CodeStatus;
 import com.example.sellservicespringproject.services.CodeService;
+import com.example.sellservicespringproject.services.SendSimpleMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
+
+import java.util.Calendar;
+import java.util.Objects;
 
 @Service
 public class CodeServiceImpl implements CodeService {
@@ -14,18 +22,55 @@ public class CodeServiceImpl implements CodeService {
     @Autowired
     private CodeRepo codeRepo;
 
+    @Autowired
+    private SendSimpleMessage sendMessage;
+
     @Override
-    public int randomCode() {
-        return (int) (Math.random() * 9000) + 1000;
+    public void saveCode(CodeDto codeDto) {
+        codeRepo.save(CodeMapper.INSTANCE.mapToCode(codeDto));
     }
 
     @Override
-    public void saveCode(Code code) {
-        codeRepo.save(code);
+    public Code findLastCode(UserDto userDto) {
+        return codeRepo.findByUserAndCodeStatus(UserMapper.INSTANCE.mapToUser(userDto), CodeStatus.NEW);
     }
 
     @Override
-    public Code findUserCode(User user) {
-        return codeRepo.findByUserAndCodeStatus(user, CodeStatus.NEW);
+    public void sendCode(UserDto userDto) {
+
+        Code lastCode =
+                codeRepo.findByUserAndCodeStatus(UserMapper.INSTANCE.mapToUser(userDto), CodeStatus.NEW);
+
+        if (Objects.nonNull(lastCode)) {
+
+            lastCode.setCodeStatus(CodeStatus.CANCELLED);
+
+            codeRepo.save(lastCode);
+        }
+
+        int code = (int) ((Math.random() * 9000) + 1000);
+
+
+        String hashedCode =
+                BCrypt
+                        .hashpw(
+                                Integer
+                                        .toString(code)
+                                , BCrypt.gensalt());
+
+        Calendar endOfCodeAction = Calendar.getInstance();
+        endOfCodeAction.add(Calendar.MINUTE, 3);
+
+        Code saveCode = new Code();
+        saveCode.setCode(hashedCode);
+        saveCode.setEndDate(endOfCodeAction.getTime());
+        saveCode.setCodeStatus(CodeStatus.NEW);
+        saveCode.setUser(UserMapper.INSTANCE.mapToUser(userDto));
+        codeRepo.save(saveCode);
+
+        sendMessage
+                .sendSimpleMessage(
+                        userDto.getEmail()
+                        , Integer.toString(code));
     }
 }
