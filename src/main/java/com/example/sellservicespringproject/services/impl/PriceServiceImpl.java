d1f47dd36d475936a3ea212e7e5aeea796a5cd80
doorbiy <tdoorbiy@gmail.com>
2021-09-30 12:34:17 +0600

    created PriceServiceImpl created methods: savePrice getPriceByProduct getAllPrices

---
+++ src/main/java/com/example/sellservicespringproject/services/impl/PriceServiceImpl.java
@@ -0,0 +1,141 @@
+package com.example.sellservicespringproject.services.impl;
+
+import com.example.sellservicespringproject.dao.PriceRepo;
+import com.example.sellservicespringproject.mappers.PriceMapper;
+import com.example.sellservicespringproject.mappers.ProductMapper;
+import com.example.sellservicespringproject.models.dtos.PriceDto;
+import com.example.sellservicespringproject.models.dtos.ProductDto;
+import com.example.sellservicespringproject.models.entities.Price;
+import com.example.sellservicespringproject.models.responses.ErrorResponse;
+import com.example.sellservicespringproject.services.PriceService;
+import com.example.sellservicespringproject.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@Service
+public class PriceServiceImpl implements PriceService {
+
+    @Autowired
+    private PriceRepo priceRepo;
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public ResponseEntity<?> savePrice(String token, PriceDto priceDto) {
+
+        ResponseEntity<?> responseEntity =
+                userService
+                        .verifyLogin(token);
+
+        if (!responseEntity.getStatusCode().equals(HttpStatus.OK)) {
+
+            return responseEntity;
+        }
+
+        List<Price> priceList =
+                priceRepo
+                        .findAllByProduct(
+                                ProductMapper
+                                        .INSTANCE
+                                        .mapToProduct(
+                                                priceDto
+                                                        .getProduct()));
+
+        if (Objects.nonNull(priceList) && !priceList.isEmpty()) {
+            priceList
+                    .stream()
+                    .filter(
+                            x -> x.getStartDate().before(priceDto.getStartDate())
+
+                                    || x.getStartDate().after(priceDto.getStartDate())
+
+                                    && x.getEndDate().before(priceDto.getEndDate())
+
+                                    || x.getEndDate().after(priceDto.getEndDate()))
+                    .forEach(x -> {
+
+                        x.setEndDate(new Date());
+                        priceRepo.save(x);
+                    });
+        }
+
+        Price price =
+                PriceMapper
+                        .INSTANCE
+                        .mapToPrice(priceDto);
+
+        price = priceRepo.save(price);
+
+        return ResponseEntity.ok(
+                PriceMapper
+                        .INSTANCE
+                        .mapToPriceDto(price));
+    }
+
+    @Override
+    public ResponseEntity<?> getPriceByProduct(String token, ProductDto productDto) {
+
+        ResponseEntity<?> responseEntity =
+                userService
+                        .verifyLogin(token);
+
+        if (!responseEntity.getStatusCode().equals(HttpStatus.OK)) {
+
+            return responseEntity;
+        }
+
+        Price price =
+                priceRepo
+                        .findByProductAndEndDateAfter(
+                                ProductMapper
+                                        .INSTANCE
+                                        .mapToProduct(productDto)
+                                , new Date());
+
+        if (Objects.isNull(price)) {
+
+            return new ResponseEntity<>(
+                    new ErrorResponse("Вы ввели некорректные данные!", null)
+                    , HttpStatus.NOT_FOUND);
+        }
+
+        return ResponseEntity.ok(
+                PriceMapper
+                        .INSTANCE
+                        .mapToPriceDto(price));
+    }
+
+    @Override
+    public ResponseEntity<?> getAllPrices(String token) {
+
+        ResponseEntity<?> responseEntity =
+                userService
+                        .verifyLogin(token);
+
+        if (!responseEntity.getStatusCode().equals(HttpStatus.OK)){
+
+            return responseEntity;
+        }
+
+        List<Price> priceList =
+                priceRepo
+                        .findAll();
+
+        return ResponseEntity.ok(
+                priceList
+                        .stream()
+                        .map(
+                                PriceMapper
+                                        .INSTANCE::mapToPriceDto)
+                        .collect(
+                                Collectors.toList()));
+    }
+}
