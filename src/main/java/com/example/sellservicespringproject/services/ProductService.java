1364c877c5ef0a102577f5265d01dcf82c91b23e
doorbiy <tdoorbiy@gmail.com>
2021-09-30 12:32:17 +0600

    changed getAllProducts method

--- src/main/java/com/example/sellservicespringproject/services/ProductService.java
+++ src/main/java/com/example/sellservicespringproject/services/ProductService.java
@@ -3,13 +3,11 @@ package com.example.sellservicespringproject.services;
 import com.example.sellservicespringproject.models.dtos.ProductDto;
 import org.springframework.http.ResponseEntity;
 
-import java.util.List;
-
 public interface ProductService {
 
     ResponseEntity<?> saveProduct(String token, ProductDto productDto);
 
     ResponseEntity<?> getProductByBarcode(String token, String barcode);
 
-    List<ProductDto> getAllProducts(String token);
+    ResponseEntity<?> getAllProducts(String token);
 }
