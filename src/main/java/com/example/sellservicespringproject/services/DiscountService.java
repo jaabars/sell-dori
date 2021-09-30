cbee7961e9e22ee2ed0bdfa80ed7453201883e9b
doorbiy <tdoorbiy@gmail.com>
2021-09-30 12:43:01 +0600

    created DiscountService created methods: saveDiscount getDiscountByProduct getAllDiscounts

---
+++ src/main/java/com/example/sellservicespringproject/services/DiscountService.java
@@ -0,0 +1,14 @@
+package com.example.sellservicespringproject.services;
+
+import com.example.sellservicespringproject.models.dtos.DiscountDto;
+import com.example.sellservicespringproject.models.dtos.ProductDto;
+import org.springframework.http.ResponseEntity;
+
+public interface DiscountService {
+
+    ResponseEntity<?> saveDiscount(String token, DiscountDto discountDto);
+
+    ResponseEntity<?> getDiscountByProduct(String token, ProductDto productDto);
+
+    ResponseEntity<?> getAllDiscounts(String token);
+}
