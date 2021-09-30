39193c4ad7add6918aaf875bdfeb9dbe174ce294
doorbiy <tdoorbiy@gmail.com>
2021-09-30 12:34:28 +0600

    created PriceService created methods: savePrice getPriceByProduct getAllPrices

---
+++ src/main/java/com/example/sellservicespringproject/services/PriceService.java
@@ -0,0 +1,14 @@
+package com.example.sellservicespringproject.services;
+
+import com.example.sellservicespringproject.models.dtos.PriceDto;
+import com.example.sellservicespringproject.models.dtos.ProductDto;
+import org.springframework.http.ResponseEntity;
+
+public interface PriceService {
+
+    ResponseEntity<?> savePrice(String token, PriceDto priceDto);
+
+    ResponseEntity<?> getPriceByProduct(String token, ProductDto productDto);
+
+    ResponseEntity<?> getAllPrices(String token);
+}
