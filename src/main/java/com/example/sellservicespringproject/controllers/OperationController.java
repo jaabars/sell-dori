3c0773a53d6cc88711742d1dade2806d1acb4733
doorbiy <tdoorbiy@gmail.com>
2021-10-05 12:28:59 +0600

    created OperationController created method provideOperation

---
+++ src/main/java/com/example/sellservicespringproject/controllers/OperationController.java
@@ -0,0 +1,22 @@
+package com.example.sellservicespringproject.controllers;
+
+import com.example.sellservicespringproject.models.dtos.InputDataForOperation;
+import com.example.sellservicespringproject.services.OperationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/operation")
+public class OperationController {
+
+    @Autowired
+    private OperationService operationService;
+
+    @PostMapping("/provideOperation")
+    public ResponseEntity<?> provideOperation(@RequestHeader String token, @RequestBody List<InputDataForOperation> sellingList ) {
+        return operationService.provideOperation(token, sellingList);
+    }
+}
