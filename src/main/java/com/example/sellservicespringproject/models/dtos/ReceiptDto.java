417163da383b7cf1b66d8b4c4e1fab262d85f893
doorbiy <tdoorbiy@gmail.com>
2021-10-05 12:14:30 +0600

    created ReceiptDto class

---
+++ src/main/java/com/example/sellservicespringproject/models/dtos/ReceiptDto.java
@@ -0,0 +1,18 @@
+package com.example.sellservicespringproject.models.dtos;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+import java.util.List;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ReceiptDto {
+
+   List<ReceiptDetailsDto> receiptDetailsDto;
+
+   double totalAmount;
+
+   String cashier;
+}
