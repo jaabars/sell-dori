1ee281f7322175d1579a72b61bfc147b325eb1b0
doorbiy <tdoorbiy@gmail.com>
2021-10-05 12:22:40 +0600

    created interface OperationRepo

---
+++ src/main/java/com/example/sellservicespringproject/dao/OperationRepo.java
@@ -0,0 +1,9 @@
+package com.example.sellservicespringproject.dao;
+
+import com.example.sellservicespringproject.models.entities.Operation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OperationRepo extends JpaRepository<Operation, Long> {
+}
