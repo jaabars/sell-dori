e02868bba2978670b1f2a4f183ab0636e678f48d
doorbiy <tdoorbiy@gmail.com>
2021-10-13 18:59:43 +0600

    added annotation Repository

--- src/main/java/com/example/sellservicespringproject/dao/RequestRepo.java
+++ src/main/java/com/example/sellservicespringproject/dao/RequestRepo.java
@@ -3,7 +3,9 @@ package com.example.sellservicespringproject.dao;
 import com.example.sellservicespringproject.models.entities.Code;
 import com.example.sellservicespringproject.models.entities.Request;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RequestRepo extends JpaRepository<Request, Long> {
 
     int countByCodeAndSuccess(Code code, boolean success);
